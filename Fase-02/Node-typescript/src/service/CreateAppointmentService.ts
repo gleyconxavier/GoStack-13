3305a4a21fa8f0a2d9f26c1b2fffd1d3efc3c4b0
gleyconxavier <gleyconxcarlos@gmail.com>
2020-08-18 21:45:28 -0400

    Atualização, nv02, backend do app

--- Fase-02/Node-typescript/src/service/CreateAppointmentService.ts
+++ Fase-02/Node-typescript/src/service/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -20,15 +21,11 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
 
-  constructor( appointmentsRepository: AppointmentsRepository ) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-    public execute({ date, provider}: Request): Appointment {
+    public async execute({ date, provider}: Request): Promise<Appointment> {
+      const appointmentsRepository = getCustomRepository(AppointmentsRepository);
       const appointmentDate = startOfHour(date);
-      const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      const findAppointmentInSameDate = await appointmentsRepository.findByDate(
           appointmentDate,
       );
 
@@ -36,10 +33,12 @@ class CreateAppointmentService {
           throw Error('This appointment is already booked');
       }
 
-      const appointment = this.appointmentsRepository.create({
+      const appointment = appointmentsRepository.create({
           provider,
           date: appointmentDate,
       });
+
+      await appointmentsRepository.save(appointment);
 
       return appointment;
 
