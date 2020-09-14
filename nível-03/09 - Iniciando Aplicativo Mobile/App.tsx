516a471d93b7dca23df2d425347f2ae24e316d26
Gleycon Xavier <gleyconxcarlos@gmail.com>
2020-09-14 00:08:30 -0400

    Register and Authentication

--- nível-03/09 - Iniciando Aplicativo Mobile/App.tsx
+++ nível-03/09 - Iniciando Aplicativo Mobile/App.tsx
@@ -47,7 +47,7 @@ const App = () => {
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
+                EDITADO.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
---
+++ nível-03/09 - Iniciando Aplicativo Mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,88 @@
+import React, { useCallback, useRef } from 'react';
+import { Image, ScrollView, KeyboardAvoidingView, Platform, View, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
+import Input from '../../components/input';
+import Button from '../../components/button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
+
+const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+    const passwordInputRef = useRef<TextInput>(null);
+    const navigation = useNavigation();
+
+    const handleSignIn = useCallback((data: Object) => {
+      console.log(data);
+    }, []);
+
+    return (
+    <>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={ Platform.OS === 'ios' ? 'padding' : undefined }
+            enabled
+        >
+            <ScrollView
+              keyboardShouldPersistTaps="handled"
+              contentContainerStyle={{ flex: 1 }}
+            >
+                <Container>
+                    <Image source={logoImg} />
+
+                    <View>
+                        <Title>Faça seu logon</Title>
+                    </View>
+
+                    <Form ref={formRef} onSubmit={handleSignIn}>
+
+                      <Input
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        keyboardType="email-address"
+                        name="email"
+                        icon="mail"
+                        placeholder="E-mail"
+                        returnKeyType="next"
+                        onSubmitEditing={() => {
+                          passwordInputRef.current?.focus();
+                        }}
+                      />
+
+                      <Input
+                        ref={passwordInputRef}
+                        name="password"
+                        icon="lock"
+                        placeholder="Senha"
+                        secureTextEntry
+                        returnKeyType="send"
+                        onSubmitEditing={ () => { formRef.current?.submitForm()} }
+                      />
+
+                    </Form>
+                    {/* removed from inner Form, caused some css bugs */}
+                    <Button onPress={ () => { formRef.current?.submitForm()} }>
+                      Entrar
+                    </Button>
+
+                    <ForgotPassword onPress={() => {}}>
+                        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                    </ForgotPassword>
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
+
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+            <Icon name="log-in" size={20} color="#ff9000" />
+            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+    </>
+    );
+};
+
+export default SignIn;
